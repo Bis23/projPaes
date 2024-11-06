56b152dcebb756731e0d3a39af180d070e8adba2
Bianca Santos <biancafs23@outlook.com>
2024-11-06 19:17:30 -0300

    Organizando os arquivos

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
-const mysql = require('mysql2');
-const db = require('./db');
+const db = require('./conexaoDB');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// Criando a conexão com o banco
-const connection = mysql.createConnection({
-  host: 'localhost',  // endereço
-  user: 'root',  //usuário
-  password: 'imtdb', // senha
-  database: 'testeDB' // nome do banco
-});
-
-// Conectando ao banco de dados
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao MySQL:', err);
-    return;
-  }
-  console.log('Conectado ao MySQL!');
-});
-
 app.get('/', (req, res) => {
+  // Manipulando o banco (testeDados)
   db.query('SELECT * FROM testeMesa', (err, results) => {
     if (err) {
       console.error('Erro na consulta:', err);
       return;
     }
+    // Enviando dados para a requisição
     res.send(results);
   });
 });
